2bee0ef63632ff03d2b9578a8617273874499776
joseporcar <joseaporcar@gmail.com>
2026-02-06 22:23:47 +0100

    added zen

--- programs/programs.nix
+++ programs/programs.nix
@@ -2,13 +2,14 @@
 {
   imports = [    
     ./git.nix
+    ./zen-browser.nix
     #./vscode.nix
     #./fastfetch.nix
     #./browser/firefox.nix
     #./terminal/kitty.nix
     #./terminal/fish.nix
   ];
-  
+
   home.packages = [
     pkgs.zapzap
   ];
---
+++ programs/zen-browser.nix
@@ -0,0 +1,11 @@
+{ pkgs, ...}: 
+# https://github.com/0xc000022070/zen-browser-flake?tab=readme-ov-file#home-manager-reference
+{
+    imports = [
+        inputs.zen-browser.homeModules.beta
+    ];
+    programs.zen-browser = { 
+        enable = true;
+        nativeMessagingHosts = [pkgs.firefoxpwa];
+    };
+}
